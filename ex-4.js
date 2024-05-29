b4355d07a0d22a2363ec4957695f804f666fe3b9
Warat <warat.sat@gmail.com>
2024-05-30 05:21:47 +0700

    Update assignment

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,6 @@ const employees = [
 ];
 
 // Start coding here
+employees[2]={name:"Alicia",age:29,hobbies:["Shopping","Reading novels"]}
+employees[3]={name:"Kody",age:19,hobbies:["Computer games","Computer games"]}
+console.log(employees)
